b2ecf3e5e7744d42a65a01299d00d707a00813ed
Parallax_kk <kuronekoya.mail@gmail.com>
2019-01-15 00:56:44 +0900

    * add A02(GrayScale) script * Add GrayScale() * Add A02 Button

---
+++ Assets/Scripts/Q01_10/A02.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class A02 : MonoBehaviour
+{
+    /// <summary>
+    /// 画像のテクスチャ
+    /// </summary>
+    private Texture2D m_Texture = null;
+
+    /// <summary>
+    /// 出力先RawImage
+    /// </summary>
+    [SerializeField]
+    private RawImage m_RawImage = null;
+
+    private void Awake()
+    {
+        if (m_RawImage == null)
+        {
+            m_RawImage = GameObject.Find("Canvas/MainPanel/RawImage").GetComponent<RawImage>();
+        }
+    }
+
+    /// <summary>
+    /// グレースケール
+    /// </summary>
+    public void GrayScale()
+    {
+        // Pixel情報取得
+        m_Texture = transform.root.GetComponent<FileManager>().m_Texture;
+        int width = m_Texture.width;
+        int height = m_Texture.height;
+        Color[] pixels = m_Texture.GetPixels();
+
+        // 書き換え用テクスチャの作成
+        Color[] change_pixels = new Color[pixels.Length];
+
+        for (int h = 0; h < height; h++)
+        {
+            for (int w = 0; w < width; w++)
+            {
+                int pxIndex = w + width * h;
+
+                // グレースケール化
+                float y = 0.2126f * pixels[pxIndex].r + 0.7152f * pixels[pxIndex].g + 0.0722f * pixels[pxIndex].b;
+                Color change_pixel = new Color(y, y, y, pixels[pxIndex].a);
+                change_pixels.SetValue(change_pixel, pxIndex);
+            }
+        }
+
+        // 書き換え用テクスチャの生成
+        Texture2D change_texture = new Texture2D(width, height, TextureFormat.RGBA32, false);
+        change_texture.filterMode = FilterMode.Point;
+        change_texture.SetPixels(change_pixels);
+        change_texture.Apply();
+
+        // テクスチャを貼り替える
+        m_Texture = change_texture;
+        var rect = new Rect(0.0f, 0.0f, m_Texture.width, m_Texture.height);
+        var pivot = new Vector2(0.5f, 0.5f);
+        var sprite = Sprite.Create(m_Texture, rect, pivot);
+        m_RawImage.texture = sprite.texture;
+        transform.root.GetComponent<FileManager>().m_Texture = m_Texture;
+    }
+}
