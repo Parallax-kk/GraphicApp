1b0c48415b715652d8850c579f4200623580b1d0
Parallax_kk <kuronekoya.mail@gmail.com>
2019-01-16 00:15:13 +0900

    * Add A05(ConnvertHSV) script * Add ConnvertHSV() * Add A05 Button

---
+++ Assets/Scripts/Q01_10/A05.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class A05 : MonoBehaviour
+{
+    /// <summary>
+    /// 画像のテクスチャ
+    /// </summary>
+    private Texture2D m_Texture = null;
+
+    /// <summary>
+    /// 出力先RawImage
+    /// </summary>
+    [SerializeField]
+    private RawImage m_RawImage = null;
+
+    private void Awake()
+    {
+        if (m_RawImage == null)
+        {
+            m_RawImage = GameObject.Find("Canvas/MainPanel/RawImage").GetComponent<RawImage>();
+        }
+    }
+
+    /// <summary>
+    /// HSV変換
+    /// </summary>
+    public void ConnvertHSV()
+    {
+        if (transform.root.GetComponent<FileManager>().m_Texture == null)
+            return;
+
+        // Pixel情報取得
+        m_Texture = transform.root.GetComponent<FileManager>().m_Texture;
+        int width = m_Texture.width;
+        int height = m_Texture.height;
+        Color[] pixels = m_Texture.GetPixels();
+
+        // 書き換え用テクスチャの作成
+        Color[] change_pixels = new Color[pixels.Length];
+
+        for (int i = 0; i < pixels.Count(); i++)
+        {
+            float h, s, v;
+
+            // RGBをHSVに変換
+            Color.RGBToHSV(pixels[i], out h, out s, out v);
+
+            // Hを反転
+            h = h + 0.5f;
+            if (h > 1.0f)
+                h -= 1.0f;
+
+            // HSVをRGBに変換
+            change_pixels[i] = Color.HSVToRGB(h, s, v);
+        }
+
+        // 書き換え用テクスチャの生成
+        Texture2D change_texture = new Texture2D(width, height, TextureFormat.RGBA32, false);
+        change_texture.filterMode = FilterMode.Point;
+        change_texture.SetPixels(change_pixels);
+        change_texture.Apply();
+
+        // テクスチャを貼り替える
+        m_Texture = change_texture;
+        var rect = new Rect(0.0f, 0.0f, m_Texture.width, m_Texture.height);
+        var pivot = new Vector2(0.5f, 0.5f);
+        var sprite = Sprite.Create(m_Texture, rect, pivot);
+        m_RawImage.texture = sprite.texture;
+        transform.root.GetComponent<FileManager>().m_Texture = m_Texture;
+    }
+}
