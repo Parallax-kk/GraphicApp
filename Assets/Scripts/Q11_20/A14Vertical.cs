73afd99735b05220e910629205078e9617d1c690
Parallax_kk <kuronekoya.mail@gmail.com>
2019-01-22 01:52:31 +0900

    Add A14Horizon() script Add DifferentialFilterHorizon() Add A14Horizon Button
    Add A14Vertical() script
    Add DifferentialFilterVertical()
    Add A14Vertical Button

---
+++ Assets/Scripts/Q11_20/A14Vertical.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class A14Vertical : MonoBehaviour
+{
+    /// <summary>
+    /// 画像のテクスチャ
+    /// </summary>
+    private Texture2D m_Texture = null;
+
+    /// <summary>
+    /// 出力先RawImage
+    /// </summary>
+    [SerializeField]
+    private RawImage m_RawImage = null;
+
+    private void Awake()
+    {
+        if (m_RawImage == null)
+        {
+            m_RawImage = GameObject.Find("Canvas/MainPanel/RawImage").GetComponent<RawImage>();
+        }
+    }
+
+    /// <summary>
+    /// 微分フィルタ
+    /// </summary>
+    public void DifferentialFilterVertical()
+    {
+        if (transform.root.GetComponent<FileManager>().m_Texture == null)
+            return;
+
+        // Pixel情報取得
+        m_Texture = transform.root.GetComponent<FileManager>().m_Texture;
+        int width = m_Texture.width;
+        int height = m_Texture.height;
+        Color[] pixels = m_Texture.GetPixels();
+
+        // 書き換え用テクスチャの作成
+        Color[] change_pixels = new Color[pixels.Length];
+
+        for (int h = 0; h < height; h++)
+        {
+            for (int w = 0; w < width; w++)
+            {
+                int pxIndex = w + width * h;
+
+                float y = 0.0f;
+
+                // 上端
+                if (h == 0)
+                {
+                    y = 0.2126f * pixels[pxIndex].r + 0.7152f * pixels[pxIndex].g + 0.0722f * pixels[pxIndex].b;
+                }
+                else
+                {
+                    y = Mathf.Abs((0.2126f * pixels[pxIndex        ].r + 0.7152f * pixels[pxIndex        ].g + 0.0722f * pixels[pxIndex        ].b)
+                      -           (0.2126f * pixels[pxIndex - width].r + 0.7152f * pixels[pxIndex - width].g + 0.0722f * pixels[pxIndex - width].b));
+                }
+
+                change_pixels[pxIndex] = new Color(y,y,y);
+            }
+        }
+
+        // 書き換え用テクスチャの生成
+        Texture2D change_texture = new Texture2D(width, height, TextureFormat.RGBA32, false);
+        change_texture.filterMode = FilterMode.Point;
+        change_texture.SetPixels(change_pixels);
+        change_texture.Apply();
+
+        // テクスチャを貼り替える
+        m_Texture = change_texture;
+        var rect = new Rect(0.0f, 0.0f, m_Texture.width, m_Texture.height);
+        var pivot = new Vector2(0.5f, 0.5f);
+        var sprite = Sprite.Create(m_Texture, rect, pivot);
+        m_RawImage.texture = sprite.texture;
+        transform.root.GetComponent<FileManager>().m_Texture = m_Texture;
+    }
+}
